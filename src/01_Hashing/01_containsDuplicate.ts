675874ad203ee5e78aadad6c4fcf134b1f5fd623
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2026-04-06 08:34:37 -0600

    Update: Renaming containsDuplicate

--- src/01_Hashing/01_containsDuplicate.ts
+++ src/01_Hashing/01_containsDuplicate.ts
@@ -9,25 +9,10 @@
  *
  * @param nums - Array of integers
  * @returns true if duplicate exists, false otherwise
+ *
+ * //[1,2,3,4,5,5] true
+ * //[1,2,3,4,5] false
+ *
  */
 
-//[1,2,3,4,5,5] true
-//[1,2,3,4,5] false
 
-function containsDuplicate(arr: number[]): boolean {
-  const seen = new Set<number>();
-
-  for (const num of arr) {
-    if (seen.has(num)) {
-      return true;
-    }
-    seen.add(num);
-  }
-  return false;
-}
-
-const arr = [1, 2, 3, 4, 5, 5]; //true
-const arr2 = [1, 2, 3, 4, 5]; //false
-
-console.log(`Tienes duplicados`, containsDuplicate(arr));
-console.log(`Tienes duplicados`, containsDuplicate(arr2));
